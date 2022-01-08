c6c8e3294a1e1e70308a268651a87cbbe531171e
atieharmin <atieharmin08@gmail.com>
2022-01-09 02:01:02 +0330

    Extract Method/Feature Envy/Duplicate Code/Repeated Switches/Replace Temp with Query - ShoppingCart Class

--- java/src/main/java/dojo/supermarket/model/Offer.java
+++ java/src/main/java/dojo/supermarket/model/Offer.java
@@ -15,4 +15,52 @@ public class Offer {
         return this.product;
     }
 
+
+    public double getDiscountAmount(double unitPrice, double quantity){
+        double basePrice = quantity * unitPrice;
+        double discount = 0;
+        switch (offerType) {
+            case TwoForAmount:
+                discount = ((int) quantity >= 2) ? getForAmountDiscount(unitPrice, (int) quantity, 2) : 0;
+                break;
+            case ThreeForTwo:
+                discount = ((int) quantity > 2) ? getThreeForTwoDiscount(unitPrice, (int) quantity) : 0;
+                break;
+            case TenPercentDiscount:
+                return quantity * unitPrice * argument / 100.0;
+            case FiveForAmount:
+                discount = ((int) quantity >= 5) ? getForAmountDiscount(unitPrice, (int) quantity,5) : 0;
+                break;
+        }
+        return basePrice - discount;
+    }
+
+    private double getThreeForTwoDiscount(double unitPrice, int quantityAsInt) {
+        return  (quantityAsInt / 3 * 2 * unitPrice) + quantityAsInt % 3 * unitPrice;
+    }
+
+    private double getForAmountDiscount(double unitPrice, int quantityAsInt, int divisor) {
+        return argument * (quantityAsInt / divisor) + quantityAsInt % divisor * unitPrice;
+    }
+
+    public String getDiscountDescription(){
+        switch (offerType) {
+            case TwoForAmount:
+                return "2 for " + argument;
+            case ThreeForTwo:
+                return "3 for 2";
+            case TenPercentDiscount:
+                return argument + "% off";
+            case FiveForAmount:
+                return "5 for " + argument;
+            default:
+                return "";
+        }
+    }
+    public Discount getOfferDiscount(Product p,double unitPrice, double quantity){
+        if(getDiscountAmount(unitPrice,quantity) == 0)
+            return null;
+        return new Discount(p, getDiscountDescription(), -getDiscountAmount(unitPrice,quantity));
+    }
+
 }
